7187a999194670e9a960b699267a20e06c3d5470
Muhamad Iqbal <ikibozu@shukaku.local>
2017-03-19 18:31:59 +0000

    hallo

--- notebooks/cw4_testing/cw4.paper.py
+++ notebooks/cw4_testing/cw4.paper.py
@@ -20,7 +20,7 @@ assert 'OUTPUT_DIR' in os.environ, (
 nonlinear_arr = 'tf.nn.relu'
 learning_rate = 0.001
 num_epoch = 100
-dropout =  [ .75, .75, .75, .75]# Dropout, probability to keep units
+dropout = 0.75 # Dropout, probability to keep units
 keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
 commands = {
     'tf.nn.relu' : tf.nn.relu,
@@ -36,14 +36,10 @@ graph = tf.Graph()
 if (str(sys.argv[1])=='10'):
     train_data = CIFAR10DataProvider('train', batch_size=50)
     valid_data = CIFAR10DataProvider('valid', batch_size=50)
-    test_inputs = np.load(os.path.join(os.environ['MLP_DATA_DIR'], 'cifar-10-test-inputs.npz'))['inputs']
-    test_inputs = test_inputs.reshape((-1, 1024, 3), order='F')
-    test_inputs = test_inputs.reshape((-1,32,32,3))
     dataset ='C10'
 if (str(sys.argv[1])=='100'):
     train_data = CIFAR100DataProvider('train', batch_size=50)
     valid_data = CIFAR100DataProvider('valid', batch_size=50)
-    test_inputs = np.load(os.path.join(os.environ['MLP_DATA_DIR'], 'cifar-100-test-inputs.npz'))['inputs']
     dataset ='C100'
 
 train_data.inputs = train_data.inputs.reshape((-1, 1024, 3), order='F')
@@ -72,47 +68,47 @@ def maxpool2d(x, k=3, s=2):
 def norm(layer, lsize) :
     return tf.nn.lrn(layer, lsize, bias=2.0, alpha=0.001 / 9.0, beta=0.75)
 
-def create_kaggle_submission_file(predictions, output_file, overwrite=False):
-    if predictions.shape != (10000, 100):
-        raise ValueError('predictions should be an array of shape (10000, 25).')
-    if not (np.all(predictions >= 0.) and 
-            np.all(predictions <= 1.)):
-        raise ValueError('predictions should be an array of probabilities in [0, 1].')
-    if not np.allclose(predictions.sum(-1), 1):
-        raise ValueError('predictions rows should sum to one.')
-    if os.path.exists(output_file) and not overwrite:
-        raise ValueError('File already exists at {0}'.format(output_file))
-    pred_classes = predictions.argmax(-1)
-    ids = np.arange(pred_classes.shape[0])
-    np.savetxt(output_file, np.column_stack([ids, pred_classes]), fmt='%d',
-               delimiter=',', header='Id,Class', comments='')
+
 
 # Store layers weight & bias
 weights = {
     # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.truncated_normal([3, 3, 3, 32], stddev=.1)),
-    'wc1a': tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev=.1)),
+    'wc0': tf.Variable(tf.truncated_normal([3, 3, 3, 32], stddev=.1)),
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=.1)),
+    'wc1b': tf.Variable(tf.truncated_normal([3, 3, 64, 128], stddev=.1)),
+    # 5x5 conv, 1 input, 32 outputs
+    'wc2': tf.Variable(tf.truncated_normal([3, 3, 128, 192], stddev=.1)),
+    'wc2b': tf.Variable(tf.truncated_normal([3, 3, 192, 240], stddev=.1)),
+    # 5x5 conv, 1 input, 32 outputs
+    'wc3': tf.Variable(tf.truncated_normal([3, 3, 240, 260], stddev=.1)),
+    'wc3b': tf.Variable(tf.truncated_normal([3, 3, 260, 280], stddev=.1)),
+    # 5x5 conv, 1 input, 32 outputs
+    'wc4': tf.Variable(tf.truncated_normal([3, 3, 280, 280], stddev=.1)),
+    'wc4b': tf.Variable(tf.truncated_normal([3, 3, 280, 300], stddev=.1)),
     # 5x5 conv, 32 inputs, 64 outputs
-    'wc2': tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=.1)),
-    'wc2a': tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=.1)),
-    'wc3': tf.Variable(tf.truncated_normal([3, 3, 64, 128], stddev=.1)),
-    'wc3a': tf.Variable(tf.truncated_normal([3, 3, 128, 128], stddev=.1)),
+    #'wc5': tf.Variable(tf.truncated_normal([3, 3, 64, 128], stddev=.1)),
+    #'wc5a': tf.Variable(tf.truncated_normal([3, 3, 128, 128], stddev=.1)),
     #'wc4': tf.Variable(tf.random_normal([3, 3, 384, 384]))
     # fully connected, 7*7*64 inputs, 1024 outputs
     #'wd1': tf.Variable(tf.random_normal([4*4*64, 4096])),
     # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.truncated_normal([2048, 2048], stddev=2. / (4096 + 2048)**0.5)),
-    'wd2': tf.Variable(tf.truncated_normal([2048, 1024], stddev=2. / (2048 + 1024)**0.5)),
+    'wd1': tf.Variable(tf.truncated_normal([1200, 300], stddev=2. / (1200 + 300)**0.5)),
+    'wd2': tf.Variable(tf.truncated_normal([300, 100], stddev=2. / (300 + 1024)**0.5)),
     # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([1024, train_data.num_classes]))
+    'out': tf.Variable(tf.random_normal([100, train_data.num_classes]))
 }
 
 biases = {
-    'bc1': tf.Variable(tf.random_normal([32])),
-    'bc2': tf.Variable(tf.random_normal([64])),
-    'bc3': tf.Variable(tf.random_normal([128])),
-    'bd1': tf.Variable(tf.random_normal([2048])),
-    'bd2': tf.Variable(tf.random_normal([1024])),
+    'bc0': tf.Variable(tf.random_normal([32])),
+    'bc1': tf.Variable(tf.random_normal([64])),
+    'bc2': tf.Variable(tf.random_normal([128])),
+    'bc3': tf.Variable(tf.random_normal([192])),
+    'bc4': tf.Variable(tf.random_normal([240])),
+    'bd1': tf.Variable(tf.random_normal([260])),
+    'bd2': tf.Variable(tf.random_normal([280])),
+    'bd3': tf.Variable(tf.random_normal([300])),
+    'bd4': tf.Variable(tf.random_normal([100])),
     'out': tf.Variable(tf.random_normal([train_data.num_classes]))
 }
 
@@ -139,13 +135,11 @@ with tf.name_scope('data'):
 #Layer 1 CONV>POOL>NORM
 ########################
 with tf.name_scope('conv-stack-1'):
-    conv1 = conv2d(inputs, weights['wc1'], biases['bc1'])
-    print "conv1.shape:", conv1.get_shape()
-    conv1 = conv2d(conv1, weights['wc1a'], biases['bc1'])
+    conv1 = conv2d(inputs, weights['wc0'], biases['bc0'])
     print "conv1.shape:", conv1.get_shape()
     h_pool_conv1 = maxpool2d(conv1, k=2)
     print "h_pool_conv1.shape:", h_pool_conv1.get_shape()
-    do_fc1 = tf.nn.dropout(h_pool_conv1, dropout[0])
+    do_fc1 = tf.nn.dropout(h_pool_conv1, dropout)
     print "do_fc1.shape:", do_fc1.get_shape()
     norm1 = norm(do_fc1, 4)
     print "norm1.shape:", norm1.get_shape()
@@ -154,60 +148,89 @@ with tf.name_scope('conv-stack-1'):
 #Layer 2 CONV>POOL>NORM
 ########################
 with tf.name_scope('conv-stack-2'):
-    conv2 = conv2d(norm1, weights['wc2'], biases['bc2'])
+    conv2 = conv2d(norm1, weights['wc1'], biases['bc1'])
     print "conv2.shape:", conv2.get_shape()
-    conv2a = conv2d(conv2, weights['wc2a'], biases['bc2'])
-    print "conv2a.shape:", conv2a.get_shape()
+    conv2a = conv2d(conv2, weights['wc1b'], biases['bc2'])
+    print "conv3.shape:", conv2a.get_shape()
     norm2 = norm(conv2a, 4)
-    print "norm2.shape:", norm2.get_shape()
+    print "norm1.shape:", norm2.get_shape()
     h_pool_conv2 = maxpool2d(norm2, k=2)
-    print "h_pool_conv2s.shape:", h_pool_conv2.get_shape()
-    do_fc2 = tf.nn.dropout(h_pool_conv2, dropout[1])
-    print "do_fc2.shape:", do_fc2.get_shape()
+    print "h_pool_conv2.shape:", h_pool_conv2.get_shape()
+    do_fc2 = tf.nn.dropout(h_pool_conv2, dropout)
+    print "do_fc1.shape:", do_fc2.get_shape()
     
 
 ########################
 #Layer 3 CONV>POOL>NORM
 ########################
 with tf.name_scope('conv-stack-3'):
-    conv3 = conv2d(do_fc2, weights['wc3'], biases['bc3'])
-    print "conv3.shape:", conv3.get_shape()
-    conv3a = conv2d(conv3, weights['wc3a'], biases['bc3'])
-    print "conv3a.shape:", conv3a.get_shape()
+    conv3 = conv2d(do_fc2, weights['wc2'], biases['bc3'])
+    print "conv2.shape:", conv2.get_shape()
+    conv3a = conv2d(conv3, weights['wc2b'], biases['bc4'])
+    print "conv3.shape:", conv2.get_shape()
     norm3 = norm(conv3a, 4)
     print "norm3.shape:", norm3.get_shape()
     h_pool_conv3 = maxpool2d(norm3, k=2)
     print "h_pool_conv3.shape:", h_pool_conv3.get_shape()
-    do_fc3 = tf.nn.dropout(h_pool_conv3, dropout[2])
-    print "do_fc1.shape:", do_fc3.get_shape()
-    pool5Shape = do_fc3.get_shape().as_list()
+    do_fc3 = tf.nn.dropout(h_pool_conv3, dropout)
+    print "do_fc3.shape:", do_fc3.get_shape()
     
 
+########################
+#Layer 4 CONV>POOL>NORM
+########################
+with tf.name_scope('conv-stack-4'):
+    conv4 = conv2d(do_fc3, weights['wc3'], biases['bd1'])
+    print "conv4.shape:", conv4.get_shape()
+    conv4a = conv2d(conv4, weights['wc3b'], biases['bd2'])
+    print "conv4a.shape:", conv4a.get_shape()
+    norm4 = norm(conv4a, 4)
+    print "norm4.shape:", norm4.get_shape()
+    h_pool_conv4 = maxpool2d(norm4, k=2)
+    print "h_pool_conv2.shape:", h_pool_conv4.get_shape()
+    do_fc4 = tf.nn.dropout(h_pool_conv4, dropout)
+    print "do_fc4.shape:", do_fc4.get_shape()
+   
+########################
+#Layer 5 CONV>POOL>NORM
+########################
+with tf.name_scope('conv-stack-5'):
+    conv5 = conv2d(do_fc4, weights['wc4'], biases['bd2'])
+    print "conv5.shape:", conv5.get_shape()
+    conv5a = conv2d(conv5, weights['wc4b'], biases['bd3'])
+    print "conv5a.shape:", conv5a.get_shape()
+    norm5 = norm(conv5a, 4)
+    print "norm5.shape:", norm5.get_shape()
+    h_pool_conv5= maxpool2d(norm5, k=2)
+    print "h_pool_conv5.shape:", h_pool_conv5.get_shape()
+    do_fc5 = tf.nn.dropout(h_pool_conv5, dropout)
+    print "do_fc5.shape:", do_fc5.get_shape()
+    pool5Shape = do_fc5.get_shape().as_list()
 
 ########################
 #Layer 3 FC
 ########################
 with tf.name_scope('fc-layer-1'):
-    flat_fc1 = tf.reshape(do_fc3, [-1, pool5Shape[1]*pool5Shape[2]*pool5Shape[3] ])#weights['wd1'].get_shape().as_list()[0]]) #3136
+    flat_fc1 = tf.reshape(do_fc5, [-1, pool5Shape[1]*pool5Shape[2]*pool5Shape[3] ])#weights['wd1'].get_shape().as_list()[0]]) #3136
     print "flat_fc1.shape:", flat_fc1.get_shape()
-    fc1 = tf.add(tf.matmul(flat_fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.add(tf.matmul(flat_fc1, weights['wd2']), biases['bd4'])
     print "fc1.shape:", fc1.get_shape()
     relu_fc1 = tf.nn.relu(fc1)
     print "relu_fc1.shape:", relu_fc1.get_shape()
     #do_fc1 = tf.nn.dropout(relu_fc1, dropout)
 
-with tf.name_scope('fc-layer-2'):
-    #flat_fc2 = tf.reshape(relu_fc1, weights['wd2'].get_shape().as_list()[0]) #3136
-    #   print "flat_fc2.shape:", flat_fc1.get_shape()
-    fc2 = tf.add(tf.matmul(relu_fc1, weights['wd2']), biases['bd2'])
-    print "fc2.shape:", fc2.get_shape()
-    relu_fc2 = tf.identity(fc2)
-    print "relu_fc2.shape:", relu_fc2.get_shape()
-    #do_fc2 = tf.nn.dropout(relu_fc2, dropout)
+#with tf.name_scope('fc-layer-2'):
+#    #flat_fc2 = tf.reshape(relu_fc1, weights['wd2'].get_shape().as_list()[0]) #3136
+#    #   print "flat_fc2.shape:", flat_fc1.get_shape()
+#    fc2 = tf.add(tf.matmul(relu_fc1, weights['wd2']), biases['bd2'])
+#    print "fc2.shape:", fc2.get_shape()
+#    relu_fc2 = tf.identity(fc2)
+#    print "relu_fc2.shape:", relu_fc2.get_shape()
+#    #do_fc2 = tf.nn.dropout(relu_fc2, dropout)
 
 with tf.name_scope('output'):
-    print "do_fc1.shape:", relu_fc2.get_shape()
-    outputs = tf.add(tf.matmul(relu_fc2, weights['out']), biases['out'])
+    print "do_fc1.shape:", relu_fc1.get_shape()
+    outputs = tf.add(tf.matmul(relu_fc1, weights['out']), biases['out'])
     print "outputs.shape:", outputs.get_shape()
 
     
@@ -237,10 +260,7 @@ train_writer = tf.summary.FileWriter(os.path.join(exp_dir, 'train-summaries'))
 valid_writer = tf.summary.FileWriter(os.path.join(exp_dir, 'valid-summaries'))
 saver = tf.train.Saver()
 
-min_val_error = 1e6
-mve_acc = 0
-lowest_epoch = 0
-stopping = 0
+
 train_accuracy = np.zeros(num_epoch)
 train_error = np.zeros(num_epoch)
 valid_accuracy = np.zeros(num_epoch)
@@ -277,17 +297,6 @@ for e in range(num_epoch):
     valid_writer.add_summary(valid_summary, step)
     # checkpoint model variables
     saver.save(sess, os.path.join(checkpoint_dir, 'model.ckpt'), step)
-    #stopping
-    if (valid_error[e] <= min_val_error):
-        lowest_epoch = e
-        min_val_error = valid_error[e]
-        mve_acc = valid_accuracy[e]
-        #test_predictions = sess.run(tf.nn.softmax(outputs), feed_dict={inputs: test_inputs})
-        #create_kaggle_submission_file(test_predictions, 'cifar-10-example-network-submission.csv', True)          
-      
-    elif (e - lowest_epoch >=20):
-        stopping = 1
-        break
     # write stats summary to stdout
     print('Epoch {0:02d}: err(train)={1:.2f} acc(train)={2:.2f} step={3:.2f} time={4:.2f}s'
           .format(e + 1, train_error[e], train_accuracy[e], step, epoch_time))
@@ -297,7 +306,7 @@ for e in range(num_epoch):
 total_run_time = time.time() - start_run_time
 print('Total time ={0:.2f}s'.format(total_run_time))
 print('End of epoch {0:02d}: err(test)={1:.4f} acc(test)={2:.4f}'
-                          .format(lowest_epoch + 1, min_val_error, mve_acc))
+                          .format(lowest_epoch + 1, test_error, test_accuracy))
 
 # close writer and session objects
 train_writer.close()
